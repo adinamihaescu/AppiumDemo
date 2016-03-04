8edd8b07f342b02403ce6f19ac422577d4bd6dbf
unknown <mihaescu.a.adina@gmail.com>
2016-03-04 17:38:05 +0200

    Added click on an item from a list

--- src/main/java/AndroidTest.java
+++ src/main/java/AndroidTest.java
@@ -1,13 +1,17 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -24,15 +28,14 @@ public class AndroidTest {
 
     public AndroidDriver getLocalAndroid() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(CapabilityType.BROWSER_NAME, ""); //Name of mobile web browser to automate. Should be an empty string if automating an app instead.
         //required for native and hybrid testing
         capabilities.setCapability("deviceName", "Android");
         //specify apk path to install the application, if not specify uses the already installed app if any
-        capabilities.setCapability("app", "D:\\AppiumTest\\app-debug.apk");
+        capabilities.setCapability("app", "D:\\EndavaTime\\PracticeMeetup\\AndroidPracticeMeetup\\app\\build\\outputs\\apk\\app-debug.apk");
         //application package
-        capabilities.setCapability("appPackage", "com.endava.tobetested");
+        capabilities.setCapability("appPackage", "com.endava.practicemeetup");
         //main Activity - used to start the application
-        capabilities.setCapability("appActivity", ".MainActivity");
+        capabilities.setCapability("appActivity", ".activity.NavigationActivity");
         //udid of the device
         capabilities.setCapability("udid", "08b001d200740ce0");
 
@@ -43,7 +46,17 @@ public class AndroidTest {
 
     @Test
     public void appiumExampleTest() throws Exception {
-        performTouchAction();
+        List<WebElement> meetupList = driver.findElements(By.id("cv_meetup_item"));
+        meetupList.get(0).click();
+        //use xPath
+       // Assert.assertEquals("Animation", el.getText());
+        /*el = driver.findElementByClassName("android.widget.TextView");
+        Assert.assertEquals("API Demos", el.getText());
+        el = driver.findElement(By.name("App"));
+        el.click();
+        List<WebElement> els = driver.findElementsByClassName("android.widget.TextView");
+        assertEquals("Activity", els.get(2).getText());*/
+
         driver.quit();
     }
 
